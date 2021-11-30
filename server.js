0fbafbf910ae6a45cb6a04d35017143a4fd43308
Sharm-Dev <sharmainetaylor@ymail.com>
2021-11-30 19:15:00 +0000

    A

--- server.js
+++ server.js
@@ -3,21 +3,34 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const stratford = require("./data/Stratford.json");
 
-app.get("/pharmacies", (request, response) => {
-  response.send(stratford.pharmacies);
+app.get("/:city/:building", (request, response) => {
+  const city = request.params.city;
+  const building = request.params.building;
+  const dataCity = require(`./data/${city}.json`);
+  response.send(dataCity[building]);
 });
 
-app.get("/colleges", (request, response) => {
-  response.send(stratford.colleges);
-});
 
-app.get("/doctors", (request, response) => {
-  response.send(stratford.doctors);
-});
+//The city dyminic string code, get request to a json file
+// app.get("/:city/pharmacies", (request, response) => {
+//     const city = request.params.city;
+//     const dataCity = require(`./data/${city}.json`)
+//   response.send(dataCity.pharmacies);
+// });
 
-app.get("/hospitals", (request, response) => {
-  response.send(stratford.hospitals);
-});
+
+// //This is single get request to a json file
+// app.get("/colleges", (request, response) => {
+//   response.send(stratford.colleges);
+// });
+
+// app.get("/doctors", (request, response) => {
+//   response.send(stratford.doctors);
+// });
+
+// app.get("/hospitals", (request, response) => {
+//   response.send(stratford.hospitals);
+// });
 
 app.listen(PORT, () => {
   console.log("listening on port" + PORT);
